20a447074d1bfd7fd247e3a592e551a91c97f7f9
Xcutor <Lekanabass50@gmail.com>
2024-01-06 21:34:29 -0600

    task 6 buf fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
     for row in matrix:
-        if len(row) == 0:
+        if not row:
             break
         for num in range(len(row)):
             print("{:d}".format(row[num]), end='')
