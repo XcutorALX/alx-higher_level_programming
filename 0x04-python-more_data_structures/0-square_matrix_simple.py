9dc19a5124fa13a768db05d408011775193005bf
Xcutor <Lekanabass50@gmail.com>
2024-01-10 12:25:36 -0600

    task done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if len(matrix) == 0:
-        return []
-    if len(matrix) != len(matrix[0]) or len(matrix) != len(matrix[1]):
-        return []
-
-    result = []
-
-    for row in matrix:
-        rowResult = []
-        for col in row:
-            rowResult.append(col ** 2)
-
-        result.append(rowResult)
-
+    result = [[col ** 2 for col in row] for row in matrix]
     return result
