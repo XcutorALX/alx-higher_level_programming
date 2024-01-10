c84a52adb797d8e87723cabe22ff94852a46c6cd
Xcutor <Lekanabass50@gmail.com>
2024-01-10 13:37:30 -0600

    task done

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    number = 0
+    values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+
+    for i in range(len(roman_string)):
+        if i > 0 and (values[roman_string[i]] > values[roman_string[i - 1]]):
+            number += values[roman_string[i]] - values[roman_string[i - 1]]
+        else:
+            number += values[roman_string[i]]
+
+    return number
