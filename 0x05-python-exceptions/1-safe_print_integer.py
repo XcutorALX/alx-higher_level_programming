b33e00ce1abcf01dbf7026f0768d642921a80a84
Xcutor <Lekanabass50@gmail.com>
2024-01-22 14:12:45 -0600

    task done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if (value is None or not value):
+    if (value is None):
         return (False)
     try:
         print("{:d}".format(value))
