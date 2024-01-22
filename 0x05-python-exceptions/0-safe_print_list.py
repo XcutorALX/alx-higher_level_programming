620ae27bd8b9a3e675f3535e249ef6784f3c907c
Xcutor <Lekanabass50@gmail.com>
2024-01-22 13:54:42 -0600

    task done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
     while (i < x):
         try:
             print(my_list[i], end='')
-        except:
+        except IndexError:
             print()
             return (i)
-        i+=1
+        i += 1
     print()
     return (i)
