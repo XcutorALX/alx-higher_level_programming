19b0417e94c067cd63646cb34de546becde1a895
Xcutor <Lekanabass50@gmail.com>
2024-01-22 20:08:45 -0600

    task done

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as err:
+        stderr.write("Exception: {}\n".format(err))
+        result = None
+    finally:
+        return (result)
+
