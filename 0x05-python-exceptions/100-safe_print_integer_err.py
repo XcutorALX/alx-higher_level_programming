4d04813d8d61a5c44b6d578e7a51806247444f42
Xcutor <Lekanabass50@gmail.com>
2024-01-22 20:02:19 -0600

    task done

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import stderr
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
@@ -11,4 +12,4 @@ def safe_print_integer_err(value):
         stderr.write("Exception: {}\n".format(err))
         return (False)
 
-    return(True)
+    return (True)
