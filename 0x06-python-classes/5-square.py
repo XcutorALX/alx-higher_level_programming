faf00476e1ddc5bf631698e7a3a4be8ee3368267
Xcutor <Lekanabass50@gmail.com>
2024-01-24 22:55:20 -0600

    tadk one

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -43,5 +43,9 @@ class Square:
 
     def my_print(self):
         """Prints the square with '#'"""
+
+        if (self.size == 0):
+            print()
+            return
         for i in range(self.__size):
             print("#" * self.__size)
