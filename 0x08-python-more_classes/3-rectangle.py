9291b2c653418f8c5d88580a4a4146d78e727c30
Xcutor <Lekanabass50@gmail.com>
2024-01-29 21:19:25 -0600

    task done

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -71,4 +71,4 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ('')
 
-        return (("#" * self.width + '\n') * self.height)
+        return ((("#" * self.width + '\n') * self.height).rstrip())
