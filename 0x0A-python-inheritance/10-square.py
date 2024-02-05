ff0fec3f68262a745a49f5cdd9d0f1413b51948c
Xcutor <Lekanabass50@gmail.com>
2024-02-05 16:34:54 -0600

    task done

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Representation of a Square
+
+    Base:
+        Rectangle: a class representation of a rectangle
+    """
+
+    def __init__(self, size):
+        super().__init__(size, size)
