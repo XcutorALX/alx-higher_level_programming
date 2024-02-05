c59ed0b064e705b8d3aebc906603b2e7f0403222
Xcutor <Lekanabass50@gmail.com>
2024-02-05 02:12:17 -0600

    task done

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains a function is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if two objects are of the same class
+
+    Args:
+        obj: the first object to test
+        a_class: class to test with
+    
+    Return: True if same class otherwise False
+    """
+
+    return (type(obj) == a_class)
