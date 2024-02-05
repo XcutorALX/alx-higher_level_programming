0580cc08cf9a2644684cb3e865309d923145731d
Xcutor <Lekanabass50@gmail.com>
2024-02-05 16:10:12 -0600

    task done

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
 
     def area(self):
         """returns the area"""
+        
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
