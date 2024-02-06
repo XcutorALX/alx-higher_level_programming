4036983296d41e4cfa9682ac50cd2ed569ab1ff3
Xcutor <Lekanabass50@gmail.com>
2024-02-06 14:31:17 -0600

    task done

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a to_json_string func"""
+import json
+
+def to_json_string(my_obj):
+    """This function returns the json representation
+    of an object
+    """
+    return (json.dumps(my_obj))
