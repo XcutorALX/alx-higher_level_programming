951b2d399e4b3446a26f0cff59f4b4cba013bfef
Xcutor <Lekanabass50@gmail.com>
2024-02-06 15:40:55 -0600

    task done

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module contains a student class"""
+
+
+class Student:
+    """Object representation of a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantizes an object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dict representation of a student instance"""
+
+        if type(attrs) == list and (type(i) == str for i in attrs):
+            res = dict()
+            for i in attrs:
+                if i in self.__dict__:
+                    res[i] = self.__dict__[i]
+
+            return (res)
+
+        return (self.__dict__)
