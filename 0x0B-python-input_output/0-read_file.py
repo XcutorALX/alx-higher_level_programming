1eb701c63ca2fcd77b0c3e3bab290413d3aa354e
Xcutor <Lekanabass50@gmail.com>
2024-02-06 14:17:13 -0600

    task done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """This function reads a text file and prints it"""
 
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end='')
