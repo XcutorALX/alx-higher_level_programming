c429d59466b405bcee5d32d2058b1113566a3115
Xcutor <Lekanabass50@gmail.com>
2024-02-12 05:00:25 -0600

    task done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,7 +83,7 @@ class Rectangle(Base):
         """String representation of a rectangle instance"""
 
         return (f"[Rectangle] ({self.id}) {self.x}/{self.y}" +
-                f"- {self.width}/{self.height}")
+                f" - {self.width}/{self.height}")
 
     def update(self, *args, **kwargs):
         """Updates all the attributes of rectangle"""
