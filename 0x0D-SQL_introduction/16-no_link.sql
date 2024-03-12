0d797834db26169c6467a336aa11a0ea4151085b
Unchained <Lekanabass40@gmail.com>
2024-03-12 13:45:58 -0700

    task done

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,4 @@
 -- lists all records of the table second_table of the database
-SELECT * FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
