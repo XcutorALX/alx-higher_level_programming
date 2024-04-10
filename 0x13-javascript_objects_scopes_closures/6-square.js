4887764c80586aaecbaff6f07cb0894769df7ba4
Xcutor <Lekanabass50@gmail.com>
2024-04-09 22:42:48 -0500

    task done

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+// This script contains a square class
+
+
+const Rectangle = require('./4-rectangle.js');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let i;
+    let charac = c === undefined ? 'X' : c;
+    
+    for (i = 0; i < this.height; i++) {
+      console.log(charac.repeat(this.width));
+    }
+  }
+};
