5d5c932684ac977a8552de60adb170ba53ba1724
Xcutor <Lekanabass50@gmail.com>
2024-04-15 23:52:40 -0500

    task done

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This script that lists all states
+from the database hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+
+    __tablename__ = "states"
+    id = Column(
+            Integer,
+            primary_key=True,
+            autoincrement=True,
+            unique=True,
+            nullable=False
+        )
+    name = Column(
+                String(128),
+                nullable=False
+            )
