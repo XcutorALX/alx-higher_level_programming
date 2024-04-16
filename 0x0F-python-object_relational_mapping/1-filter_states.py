fe297d0231f00866cce6f7d59b4d77c8a7abd217
Xcutor <Lekanabass50@gmail.com>
2024-04-15 22:53:20 -0500

    task done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" This script that lists all states from the database hbtn_0e_0_usa:
+""" This script that lists all states 
+from the database hbtn_0e_0_usa
 """
 
 
@@ -10,7 +11,8 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(
             host='localhost',
-            port=3306, user=sys.argv[1],
+            port=3306,
+            user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
             charset="utf8"
