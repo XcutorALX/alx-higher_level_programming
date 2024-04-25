6ee2633eff00dd13734cf96d832bd02956c6a072
Xcutor <Lekanabass50@gmail.com>
2024-04-25 18:06:22 -0500

    task done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Script that sends an http request and displays the length of the response
-curl -sI $1 | grep "Content-Length"
+curl -sI $1 | grep "Content-Length" | grep -oP "\d+"
