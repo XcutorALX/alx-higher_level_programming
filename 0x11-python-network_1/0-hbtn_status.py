ae88cf5ef9d23ef931dde655eb70b8306d86ecc0
Xcutor <Lekanabass50@gmail.com>
2024-04-26 10:37:08 -0500

    task done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@
 from urllib.request import urlopen
 
 
-with urlopen("https://alx-intranet.hbtn.io/status") as response:
-    message = response.read()
-    print("Body response:")
-    print(f"    - type: {type(message)}")
-    print(f"    - content: {message}")
-    print(f"    - utf8 content: {message.decode('utf-8')}")
+if __name__ == "__main__":
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        message = response.read()
+        print("Body response:")
+        print(f"    - type: {type(message)}")
+        print(f"    - content: {message}")
+        print(f"    - utf8 content: {message.decode('utf-8')}")
