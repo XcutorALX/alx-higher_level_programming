e67563cf71163bdbdf8a23f9ea3947f1b20c84c1
Xcutor <Lekanabass50@gmail.com>
2024-04-26 11:05:54 -0500

    task done

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This script uses urllib module to diplay the result of fetching a url
+"""
+if __name__ == "__main__":
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+    from sys import argv
+    
+    data = {"email": argv[2]}
+    data = urlencode(data).encode('utf-8')
+    req = Request(argv[1], data=data, method="POST")
+    with urlopen(req) as response:
+        print(response.read().decode('utf-8'))
