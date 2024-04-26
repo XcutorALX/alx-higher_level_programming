48294bd9261f62bb61dea2b3803853de51ee627c
Xcutor <Lekanabass50@gmail.com>
2024-04-25 19:07:01 -0500

    task done

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Function: find_peak(listint)
+"""
+
+
+def find_peak(list_of_integers):
+    """ finds the peak in a list of integers"""
+    if not list_of_integers:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
+
