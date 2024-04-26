e1fe676b1209f139feef6ea24cebaed714773bfa
Xcutor <Lekanabass50@gmail.com>
2024-04-26 10:41:10 -0500

    task done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# This script uses urllib module to diplay the result of fetching a url
-from urllib.request import urlopen
-
-
+"""
+This script uses urllib module to diplay the result of fetching a url
+"""
 if __name__ == "__main__":
+    from urllib.request import urlopen
     with urlopen("https://alx-intranet.hbtn.io/status") as response:
         message = response.read()
         print("Body response:")
