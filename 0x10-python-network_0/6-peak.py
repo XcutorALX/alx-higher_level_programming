f859fa296af009636235b36af8621a9791ad964d
Xcutor <Lekanabass50@gmail.com>
2024-04-25 19:22:05 -0500

    task done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,27 @@
-#!/usr/bin/python3
-"""
-    Function: find_peak(listint)
-"""
+def find_peak(listint):
+    """
+    Finds a peak in a list of unsorted integers using recursive binary search.
 
+    Args:
+        listint (list): List of unsorted integers.
 
-def find_peak(list_of_integers):
-    """ finds the peak in a list of integers"""
-    if not list_of_integers:
+    Returns:
+        int: The peak element.
+    """
+    # Base case: if the list is empty, return None
+    if not listint:
         return None
 
-    left = 0
-    right = len(list_of_integers) - 1
+    # Find the middle index
+    mid = len(listint) // 2
 
-    while left < right:
-        mid = (left + right) // 2
-
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-
-    return list_of_integers[left]
-
+    # Compare the middle element with its neighbors (if they exist)
+    if (mid == 0 or listint[mid - 1] <= listint[mid]) and \
+       (mid == len(listint) - 1 or listint[mid + 1] < listint[mid]):
+        return listint[mid]
+    elif mid > 0 and listint[mid - 1] > listint[mid]:
+        # If the left neighbor is greater, recursively search the left half
+        return find_peak(listint[:mid])
+    else:
+        # Otherwise, recursively search the right half
+        return find_peak(listint[mid:])
