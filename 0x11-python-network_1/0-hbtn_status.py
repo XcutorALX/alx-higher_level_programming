23f047f8fe5e7810f926943caa36a733b19ca479
Xcutor <Lekanabass50@gmail.com>
2024-04-26 10:46:57 -0500

    task done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,12 @@
 This script uses urllib module to diplay the result of fetching a url
 """
 if __name__ == "__main__":
-    import urllib
+    import urllib.request
 
     urlopen = urllib.request.urlopen
     with urlopen("https://alx-intranet.hbtn.io/status") as response:
         message = response.read()
         print("Body response:")
-        print(f"    - type: {type(message)}")
-        print(f"    - content: {message}")
-        print(f"    - utf8 content: {message.decode('utf-8')}")
+        print(f"\t- type: {type(message)}")
+        print(f"\t- content: {message}")
+        print(f"\t- utf8 content: {message.decode('utf-8')}")
