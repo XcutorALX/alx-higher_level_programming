9d150a6e6e70bf28cf78f3daee4e9d03202f2857
Xcutor <Lekanabass50@gmail.com>
2024-04-26 12:42:56 -0500

    task done

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script uses urllib module to diplay the result of fetching a url
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    r_json = r.json()
+    if r_json == {}:
+        print("None")
+    else:
+        print("{}".format(r_json.get('id')))
