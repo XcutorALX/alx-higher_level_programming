f02614eb6883e8b2a4ad04246648a5815ceb5c8b
Xcutor <Lekanabass50@gmail.com>
2024-05-21 21:51:41 -0500

    task done

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// A script that displays the status code of a GET request
+
+const request = require('request');
+const url = process.argv[2];
+
+const out = {};
+let key;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    const result = JSON.parse(body);
+    for (let i = 0; i < result.length; i++) {
+      key = result[i].userId;
+      out[key] = key in out ? out[key] + 1 : 1;
+    }
+    console.log(out);
+  }
+});
